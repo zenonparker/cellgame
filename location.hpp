c8b9869e83d141330598d23026627b3fcf7ecfc9
Zenon Parker <zenonparker@gmail.com>
2014-03-01 19:45:56 -0500

    Added start of location type

---
+++ location.hpp
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////
+///
+/// @file location.hpp
+/// @brief Represents a location on the Cell game board (grid).
+/// @author Zenon Parker
+/// @author Matthew Avery
+/// @date 2014
+///
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef CELL_LOCATION_HPP
+#define CELL_LOCATION_HPP
+
+#include <iostream>
+
+namespace cell {
+
+struct Location {
+  Location(int xin, int yin) : x(xin), y(yin) {}
+  int x;
+  int y;
+};
+
+bool operator==(const Location& l1, const Location& l2) {
+  return (l1.x == l2.x && l1.y == l2.y);
+}
+
+bool operator<(const Location& l1, const Location& l2) {
+  return (l1.x < l2.x ? true :
+            l1.x == l2.x && l1.y < l2.y ? true : 
+              false);
+}
+
+std::ostream& operator<<(std::ostream& out, const Location& l) {
+  out << "(" << l.x << ", " << l.y << ")";
+}
+
+} // end namespace cell
+
+#endif // CELL_LOCATION_HPP
+
