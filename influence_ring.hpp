c762e60b0be7666603071f215d031937a8779a92
Zenon Parker <zenonparker@gmail.com>
2014-03-02 20:08:07 -0500

    Added InfluenceRing operators

--- influence_ring.hpp
+++ influence_ring.hpp
@@ -32,11 +32,34 @@ public:
   const std::vector<int>& vals() const { return vals_; }
   std::vector<int>& vals() { return vals_; }
 
+  // Operators
+  friend bool operator==(const InfluenceRing& ir1, const InfluenceRing& ir2);
+  friend std::ostream& operator<<(std::ostream& out, const InfluenceRing& ir);
+
 private:
 
-  std::vector<int> vals_{NUM_DIRECTIONS, 0};
+  std::vector<int> vals_ = std::vector<int>(NUM_DIRECTIONS, 0);
 
 };
+
+bool operator==(const InfluenceRing& ir1, const InfluenceRing& ir2) {
+  for (int i = 0; i < ir1.vals_.size(); ++i) {
+    if (ir1.vals_[i] != ir2.vals_[i]) return false;
+  }
+  return true;
+}
+
+std::ostream& operator<<(std::ostream& out, const InfluenceRing& ir) {
+  out << "(";
+  auto it = ir.vals_.begin();
+  while (it != ir.vals_.end()) {
+    out << *it++;
+    while (it != ir.vals_.end()) {
+      out << ", " << *it++;
+    }
+  }
+  return out << ")";
+}
 
 } // end namespace cell
 
--- scan.hpp
+++ scan.hpp
@@ -28,11 +28,31 @@ public:
   const std::vector<InfluenceRing>& rings() const { return rings_; }
   std::vector<InfluenceRing>& rings() { return rings_; }
 
+  // Operators
+  friend bool operator==(const Scan& s1, const Scan& s2);
+  friend std::ostream& operator<<(std::ostream& out, const Scan& s);
+
 private:
 
   std::vector<InfluenceRing> rings_{NUM_RINGS, InfluenceRing{}};
 
 };
+
+bool operator==(const Scan& s1, const Scan& s2) {
+  for (int i = 0; i < s1.rings_.size(); ++i) {
+    if (!(s1.rings_[i] == s2.rings_[i])) return false;
+  }
+  return true;
+}
+
+std::ostream& operator<<(std::ostream& out, const Scan& s) {
+  out << "{ ";
+  for (int i = 0; i < s.rings_.size(); ++i) {
+    if (i > 0) out << "\n  ";
+    out << "[" << i << "] " << s.rings_[i];
+  }
+  return out << " }";
+}
 
 } // end namespace cell
 
